989ae43fef4bccbf562699590510d4d632e11eac
OlyaBukina <katniss30051991@gmail.com>
2022-09-01 14:51:54 +0300

    Corrected JS file

--- src/script.js
+++ src/script.js
@@ -52,68 +52,35 @@ let monthYear = document.querySelector("#current-month-year");
 monthYear.innerHTML = getMonthYear(curranteDate);
 
 // City name
-function getCityName() {
-  let searchInput = document.querySelector("#search-input");
-  return searchInput.value;
-}
-
-function changeCityName() {
-  let currentCity = document.querySelector("#current-city");
-  currentCity.innerHTML = getCityName();
-}
-
 function eventHandler(e) {
   e.preventDefault();
-  changeCityName();
-  updateInfo();
+  let city = document.querySelector("#search-input").value;
+  return updateInfo(city);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", eventHandler);
 
 // Weather API
-const apiKey = "5dbca037f9f2d02bb53f691a0a9ec0f7";
 
-function updateInfo() {
-  let searchInput = document.querySelector("#current-city");
-  let city = searchInput.innerHTML;
-
+function updateInfo(city) {
+  const apiKey = "5dbca037f9f2d02bb53f691a0a9ec0f7";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  function responseHandler(response) {
-    changeContent(response);
-  }
-  axios.get(apiUrl).then(responseHandler);
+  axios.get(apiUrl).then(changeContent);
 }
 function changeContent(response) {
-  changeCurTemp(response);
-  changeHumidity(response);
-  changeWind(response);
-  changeDescription(response);
+  document.querySelector("#current-city").innerHTML = response.data.name;
+  document.querySelector("#current-temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
 }
 
-function changeCurTemp(response) {
-  let curTemp = Math.round(response.data.main.temp);
-  let h2 = document.querySelector("#current-temp");
-  h2.innerHTML = curTemp;
-}
-
-function changeHumidity(response) {
-  let curHim = response.data.main.humidity;
-  let title = document.querySelector("#humidity");
-  title.innerHTML = curHim;
-}
-
-function changeWind(response) {
-  let curWind = Math.round(response.data.wind.speed);
-  let title = document.querySelector("#wind");
-  title.innerHTML = curWind;
-}
-
-function changeDescription(response) {
-  let curDescr = response.data.weather[0].main;
-  let title = document.querySelector("#description");
-  title.innerHTML = curDescr;
-}
 // Bonus point
 function onButtonClickHandler() {
   navigator.geolocation.getCurrentPosition(resetToCurPosition);
@@ -121,20 +88,14 @@ function onButtonClickHandler() {
 function resetToCurPosition(position) {
   let curLat = position.coords.latitude;
   let curLon = position.coords.longitude;
+  const apiKey = "5dbca037f9f2d02bb53f691a0a9ec0f7";
+
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${curLat}&lon=${curLon}&appid=${apiKey}&units=metric`;
 
-  function responseHandler(response) {
-    changeCurCity(response);
-    changeContent(response);
-  }
-  axios.get(apiUrl).then(responseHandler);
+  axios.get(apiUrl).then(changeContent);
 }
 
 let curButton = document.querySelector("#btn-current");
 curButton.addEventListener("click", onButtonClickHandler);
 
-function changeCurCity(response) {
-  let curCity = response.data.name;
-  let title = document.querySelector("#current-city");
-  title.innerHTML = curCity;
-}
+updateInfo("Paris");
